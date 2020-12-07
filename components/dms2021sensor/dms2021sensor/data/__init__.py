a2c6064af6d021bb77c7910331892a6848595161
Fran <fran@serviciocorreo.com>
2020-12-07 22:44:09 +0100

    ReOrdenacion de Ficheros de sensores #1

---
+++ components/dms2021sensor/dms2021sensor/data/__init__.py
@@ -0,0 +1,4 @@
+from .sensor1 import SensorMem
+from .sensor2 import SensorSwap
+from .sensor3 import SensorArchivoX
+from .sensorAbs import Sensor
--- components/dms2021sensor/dms2021sensor/logic/__init__.py
+++ components/dms2021sensor/dms2021sensor/logic/__init__.py
@@ -1,6 +1,3 @@
 """ Sensor logic classes
 """
-from .sensorAbs import Sensor
-from .sensor1 import SensorMem
-from .sensor2 import SensorSwap
-from .sensor3 import SensorArchivoX
+
