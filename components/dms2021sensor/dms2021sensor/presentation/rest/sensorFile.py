37f53f10bb75176a5c84e0cd753fdcc54605c845
Fran <fran@serviciocorreo.com>
2020-12-15 00:20:56 +0100

    Actualizado el REST Sensor #1

--- components/dms2021sensor/dms2021sensor/presentation/rest/sensorFile.py
+++ components/dms2021sensor/dms2021sensor/presentation/rest/sensorFile.py
@@ -1,14 +1,17 @@
 from dms2021sensor.logic.sensor1 import SensorMem
 from dms2021auth.logic import UserManager
+from dms2021core.data.rest import RestResponse
 
 class sensorService():
     def __init__(self):
         pass
 
-    def create(self, sensor_type : int) -> RestResponse:
+    def monitorizarSensor(self, sensor_type : int) -> RestResponse:
         try:
-            sensor = SensorMem()
+            if sensor_type==1:
+                sensor = SensorMem()
+                print(sensor)
         except ValueError:
-            pass
+            return RestResponse(code=400,mime_type='text/plain')
 
-        return RestResponse(sensor.obtenerMemUsada())
+        return RestResponse(mime_type='text/plain')
