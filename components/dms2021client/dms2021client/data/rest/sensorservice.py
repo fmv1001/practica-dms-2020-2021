f47e9bfebb1606ed38c7700fba58fea78aadda6b
Guillermo <gss1001@alu.ubu.es>
2020-12-18 23:14:44 +0100

    Creación del servicio de sensores para el cliente #1

---
+++ components/dms2021client/dms2021client/data/rest/sensorservice.py
@@ -0,0 +1,59 @@
+""" AuthService class module.
+"""
+
+import json
+from urllib.parse import urlencode
+from http.client import HTTPConnection, HTTPResponse, HTTPException
+from dms2021client.data.rest.exc import InvalidCredentialsError, UnauthorizedError
+
+
+class SensorService():
+    """ REST client to connect to the sensor service.
+    """
+
+    def __init__(self, host: str, port: int):
+        """ Constructor method.
+
+        Initializes the client.
+        ---
+        Parameters:
+            - host: The authentication service host string.
+            - port: The authentication service port number.
+        """
+        
+        self.__host: str = host
+        self.__port: int = port
+
+    def __get_connection(self) -> HTTPConnection:
+        """ Creates a new connection to the authentication server.
+        ---
+        Returns:
+            The connection object.
+        """
+        return HTTPConnection(self.__host, self.__port)
+
+    def is_running(self) -> bool:
+        """ Tests whether the authentication service is running or not.
+        ---
+        Returns:
+            True if the sensor service could be contacted successfully; false otherwise.
+        """
+        try:
+            connection: HTTPConnection = self.__get_connection()
+            connection.request('GET', '/')
+            response: HTTPResponse = connection.getresponse()
+            if response.status == 200:
+                return True
+            return False
+        except HTTPException:
+            return False
+        except ConnectionRefusedError:
+            return False
+
+    def create_sensor(self):
+        connection: HTTPConnection = self.__get_connection()
+        connection.request('GET', '/consultarSensor')
+        response: HTTPResponse = connection.getresponse()
+        if response.status == 200:
+            print("MUY BIEN!!!")
+
