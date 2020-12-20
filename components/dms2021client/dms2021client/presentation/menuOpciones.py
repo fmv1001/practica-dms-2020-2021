c3d01fdee0cab2b9166bc7d1ea42296638eb5404
fmv1001 <fmv1001@github.com>
2020-12-20 04:44:56 +0100

    Mejora en apariencia del menu

--- components/dms2021client/dms2021client/presentation/menuOpciones.py
+++ components/dms2021client/dms2021client/presentation/menuOpciones.py
@@ -1,23 +1,26 @@
+import os
 class menuDeOpciones():
 
     def __init__(self):
         return
 
     def menu(self):
-        print("+-----------------------------------------------------------------------+")
-        print("| MENU PRINCIPAL -->                                                    |")
+        os.system("clear")
+        print("\x1b[1;36m" + "+-----------------------------------------------------------------------+")
+        print("| MENU PRINCIPAL -->                                      appGuilleFran"+ chr(174)+"|")
         print("+-----------------------------------------------------------------------+")
         print("|        1.Crear un usuario.                                            |")
         print("|        2.Cambiar permisos.                                            |")
         print("|        3.Obtener los resultados de un sensor.                         |")
+        print("|        4.Actualizar los resultados de un sensor.                      |")
         print("|        0...SALIR...                                                   |")
         print("+-----------------------------------------------------------------------+")
-        print("\n")
+        print('\033[0;m' + "\n")
         
         opcion = -1
-        while(opcion >3 or opcion < 0):
+        while(opcion >4 or opcion < 0):
             opcion  = int(input("\tIntroduce la opcion a la que desees acceder:"))
-            if(opcion >3 or opcion < 0):
+            if(opcion >4 or opcion < 0):
                 print("ERROR --> Porfavor introduce una opción de entre las que se encuentren disponibles.")
 
         return opcion
